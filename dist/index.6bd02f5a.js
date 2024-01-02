f93f93b2afeb9cfa0b6ae48a28b72e42ec5f740a
Aman Pandey <aman.pandey@axtria.com>
2024-01-02 16:26:11 +0530

    added parcel

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,9 @@
+const heading = React.createElement("h1", {
+    id: "heading",
+    class: "heading",
+    xyz: "abc"
+}, "Hello World From React");
+const root = ReactDOM.createRoot(document.querySelector(".root"));
+root.render(heading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
